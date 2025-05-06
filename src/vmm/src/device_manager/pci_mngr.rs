2beeebe18c7e8d9991c442226e1fdc478207f983
Babis Chalios <bchalios@amazon.es>
2025-05-06 17:32:40 +0200

    pci: add support for PCIe segment
    Add a PCIe segment which includes a single PCIe root port and a bus.
    At the moment, we always enabled it.
    
    At the moment, the PCIe segment is always enabled. Later commit will
    make it optional and enable it only when a command line argument flag is
    passed to Firecracker binary.
    
    Signed-off-by: Babis Chalios <bchalios@amazon.es>

---
+++ src/vmm/src/device_manager/pci_mngr.rs
@@ -0,0 +1,49 @@
+// Copyright 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::sync::Arc;
+
+use vm_device::BusError;
+
+use super::resources::ResourceAllocator;
+use crate::devices::pci::PciSegment;
+
+#[derive(Debug, Default)]
+pub struct PciDevices {
+    /// PCIe segment of the VMM, if PCI is enabled. We currently support a single PCIe segment.
+    pub pci_segment: Option<PciSegment>,
+}
+
+#[derive(Debug, thiserror::Error, displaydoc::Display)]
+pub enum PciManagerError {
+    /// Resource allocation error: {0}
+    ResourceAllocation(#[from] vm_allocator::Error),
+    /// Bus error: {0}
+    Bus(#[from] BusError),
+}
+
+impl PciDevices {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn attach_pci_segment(
+        &mut self,
+        resource_allocator: &Arc<ResourceAllocator>,
+    ) -> Result<(), PciManagerError> {
+        // We only support a single PCIe segment. Calling this function twice is a Firecracker
+        // internal error.
+        assert!(self.pci_segment.is_none());
+
+        let irqs = resource_allocator.allocate_gsi(8)?;
+        let mut pci_irq_slots = [0u8; 32];
+        for i in 0..32 {
+            pci_irq_slots[i] = irqs[i % 8].try_into().unwrap();
+        }
+
+        let pci_segment = PciSegment::new(0, resource_allocator, &pci_irq_slots)?;
+        self.pci_segment = Some(pci_segment);
+
+        Ok(())
+    }
+}
